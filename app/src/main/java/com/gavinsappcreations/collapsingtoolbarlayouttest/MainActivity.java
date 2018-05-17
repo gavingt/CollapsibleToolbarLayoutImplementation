7f408e74d3332e864eeeb88e83586437e791f288
Gavin Wright <gavingt@gmail.com>
2018-05-16 23:00:48 -0700

    Implemented CollapsedToolbarLayout

---
+++ app/src/main/java/com/gavinsappcreations/collapsingtoolbarlayouttest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gavinsappcreations.collapsingtoolbarlayouttest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Order history");
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
